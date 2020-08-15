fbdc53f838f950f8dbd380aa99a1e5ce493cd5c8
PhilKes <philk97@web.de>
2020-08-15 16:04:51 +0200

    renamed Sketches

--- RGB_Backlight_FFT/RGB_Backlight_FFT.ino
+++ RGB_Backlight_FFT/RGB_Backlight_FFT.ino
@@ -8,11 +8,17 @@
 #define OLED_RESET 4                                  //OLED reset, not hooked up but required by library
 Adafruit_SSD1306 display(OLED_RESET);                 //declare instance of OLED library called display
 
+/* OLED <-> Arduino Nano
+   VCC - 5V
+   GND - GND
+   SCK - A5 
+   SDA - A4
+*/
 char im[128], data[128];                              //variables for the FFT
 char x = 0, ylim = 30;                                //variables for drawing the graphics
 int i = 0, val;                                       //counters
 
-#define DEBUG 0
+#define DEBUG 1
 
 void setup() {
   #if DEBUG
@@ -30,7 +36,7 @@ void loop() {
   int min=1024, max=0;                                //set minumum & maximum ADC values
   for (i = 0; i < 128; i++) {                         //take 128 samples
     val = analogRead(A0);                             //get audio from Analog 0
-    data[i] = val ;                          //each element of array is val/4-128
+    data[i] = val/4-128 ;                          //each element of array is val/4-128
     im[i] = 0;                                        //
     if(val>max) max=val;                              //capture maximum level
     if(val<min) min=val;                              //capture minimum level
